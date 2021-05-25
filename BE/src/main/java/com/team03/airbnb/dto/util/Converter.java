d3a4409c029b550ca950e3f58294998e1a9e2b2d
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-05-25 16:13:32 +1200

    Fix: Converter 널처리

--- BE/src/main/java/com/team03/airbnb/dto/util/Converter.java
+++ BE/src/main/java/com/team03/airbnb/dto/util/Converter.java
@@ -8,6 +8,9 @@ public class Converter {
     private Converter() {
     }
     public static List<String> toList(String url) {
+        if(url == null) {
+            return null;
+        }
        List<String> list = Arrays.asList(url.split(", "));
        return list;
     }
