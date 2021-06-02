cc688f5e0acdd02db7fd3739ec7a544c31b0d727
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-06-02 19:46:00 +1200

    Configure: CORS 해결

---
+++ BE/src/main/java/com/team03/airbnb/WebConfig.java
@@ -0,0 +1,14 @@
+package com.team03.airbnb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+}
